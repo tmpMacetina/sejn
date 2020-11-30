592f7e313f1adc5c0ddae19fea120bd852e9488d
BookStore <ivansmgo@gmail.com>
2020-11-30 12:52:39 +0100

    Updates

---
+++ precache-manifest.7a4cf6982d50696ca2da48234a9a5bf4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82442281d1309c00da91f378012c5519",
+    "url": "/sejn/index.html"
+  },
+  {
+    "revision": "5ffaada534d261c07373",
+    "url": "/sejn/static/css/main.3fbdd6cb.chunk.css"
+  },
+  {
+    "revision": "2082f3f950b968578d09",
+    "url": "/sejn/static/js/2.0211c4fe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sejn/static/js/2.0211c4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffaada534d261c07373",
+    "url": "/sejn/static/js/main.436df857.chunk.js"
+  },
+  {
+    "revision": "be478d14cf49436799ca",
+    "url": "/sejn/static/js/runtime-main.f945150a.js"
+  },
+  {
+    "revision": "fa254f01a19f95e5f02f63677f0284b4",
+    "url": "/sejn/static/media/cotton-min.fa254f01.png"
+  },
+  {
+    "revision": "f5ca48a7fa740d19c7acfa9f61d036e1",
+    "url": "/sejn/static/media/forHerMin.f5ca48a7.jpg"
+  },
+  {
+    "revision": "b77800324b04ed021010f982a4e8e128",
+    "url": "/sejn/static/media/forHerNew.b7780032.png"
+  },
+  {
+    "revision": "5b1d64abeeafdb787a3bd1d45c584785",
+    "url": "/sejn/static/media/forHimImage.5b1d64ab.png"
+  },
+  {
+    "revision": "bc640ffd89230dc7da7262d6effaf9ed",
+    "url": "/sejn/static/media/forHimMin.bc640ffd.jpg"
+  },
+  {
+    "revision": "7037fd2b1a3949d6cbc12d3620d7e799",
+    "url": "/sejn/static/media/lady.7037fd2b.png"
+  }
+]);
